0da14f7301fed7da3c1134435cbe0dd5f2c5a707
Cheppar <ejioforched@gmail.com>
2023-07-04 20:52:27 +0300

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -7,15 +7,15 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_URL,
+const firebaseConfig = {
+  apiKey: "AIzaSyDjowPw86BEMtYpoZhFGmcnxy2_FT1dd88",
+  authDomain: "chepparing.firebaseapp.com",
+  databaseURL: "https://chepparing-default-rtdb.firebaseio.com",
+  projectId: "chepparing",
+  storageBucket: "chepparing.appspot.com",
+  messagingSenderId: "28110524265",
+  appId: "1:28110524265:web:f893e4875e901d8167f7f8",
+  measurementId: "G-ZBK4ZQNBC0"
 };
 
 // Initialize Firebase
